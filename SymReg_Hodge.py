2dc1ca68ac492b0d59baf7828c8cd292801a2e3c
TomasSilva <tomsantos.1414@gmail.com>
2023-10-01 21:48:37 -0300

    new symreg model

--- SymReg_Hodge.py
+++ SymReg_Hodge.py
@@ -1,7 +1,8 @@
 import numpy as np
-from ast import literal_eval
 from pysr import PySRRegressor
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_absolute_error
 
 #Import sasakian hodge
 Sweights, SHodge = [], []
@@ -15,17 +16,24 @@ SHodge  = np.array(SHodge)
 
 outputs = SHodge[:,1]
 
+X_train, X_test, y_train, y_test = train_test_split(Sweights, outputs, test_size=0.1, random_state=42)
 
 model = PySRRegressor(
     binary_operators=["+", "-", "*", "/"], 
-    niterations = 30,
+    niterations = 100000,
     populations = 3*os.cpu_count(),
-    ncyclesperiteration = 5000,
-    parsimony=0.0001,
+    ncyclesperiteration = 500,
+    parsimony=0.001,
     weight_optimize = 0.001,
+    maxsize = 25,
     progress=True,
     loss="L2DistLoss()",
     batching=True,
     turbo = True,
 )
-model.fit(Sweights, outputs)
+model.fit(X_train, y_train)
+
+print("##################################")
+print("R2:", r2_score(np.round(model.predict(X_test)), y_test))
+print("MAE:", mean_absolute_error(np.round(model.predict(X_test)), y_test))
+print("##################################")
